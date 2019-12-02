f1c71a806ddcb17657e20e5f7ead7ad502f277b8
honorable-exception-1170 <aliciachao@gmail.com>
2019-12-02 07:53:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ require 'directors_database'
 
 def directors_totals(nds)
   require 'pp'
-  pp directors_totals(nds)
+  pp nds
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
